f0fe4c47895d3469c0b56e98b22a100bd7c48606
whlucas <1213372700@qq.com>
2018-11-28 16:27:24 +0800

    jquery-ajax

---
+++ ajax/ajax.php
@@ -0,0 +1,18 @@
+<?php
+
+    echo $_POST['user'].'_'.$_POST['email'];
+
+
+// // 如果传进来的键值对里面的url=dy就返回这个
+// if($_POST["url"]=='dy'){
+//     // 可以接收第二个参数post过来的请求然后补全字符串发送回去给前端
+//     echo '<ul>
+//         <li>111</li> 
+//         <li>222</li>
+//         <li>333</li>
+//     </ul>';
+// }else{
+//     echo 'muyou';
+// }
+    
+?>
---
+++ ajax/ajaxCrossDomain.php
@@ -0,0 +1,8 @@
+<?php
+
+    // 我用php生成一个json数据
+    $_arr = array('a'=>1, 'b'=>2, 'c'=>3);
+    $_result = json_encode($_arr);
+    echo $_result;
+
+?>
